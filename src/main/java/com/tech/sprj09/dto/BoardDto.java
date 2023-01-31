7e6b9449cd6485c910f8cac68817b88c24d29837
tbasic5 <tbasicapp@gmail.com>
2023-01-31 14:24:55 +0900

    col step1

--- src/main/java/com/tech/sprj09/dto/BoardDto.java
+++ src/main/java/com/tech/sprj09/dto/BoardDto.java
@@ -44,61 +44,61 @@ public class BoardDto {
 	
 
 
-//
-//	public int getBid() {
-//		return bid;
-//	}
-//	public void setBid(int bid) {
-//		this.bid = bid;
-//	}
-//	public String getBname() {
-//		return bname;
-//	}
-//	public void setBname(String bname) {
-//		this.bname = bname;
-//	}
-//	public String getBtitle() {
-//		return btitle;
-//	}
-//	public void setBtitle(String btitle) {
-//		this.btitle = btitle;
-//	}
-//	public String getBcontent() {
-//		return bcontent;
-//	}
-//	public void setBcontent(String bcontent) {
-//		this.bcontent = bcontent;
-//	}
-//	public Timestamp getBdate() {
-//		return bdate;
-//	}
-//	public void setBdate(Timestamp bdate) {
-//		this.bdate = bdate;
-//	}
-//	public int getBhit() {
-//		return bhit;
-//	}
-//	public void setBhit(int bhit) {
-//		this.bhit = bhit;
-//	}
-//	public int getBgroup() {
-//		return bgroup;
-//	}
-//	public void setBgroup(int bgroup) {
-//		this.bgroup = bgroup;
-//	}
-//	public int getBstep() {
-//		return bstep;
-//	}
-//	public void setBstep(int bstep) {
-//		this.bstep = bstep;
-//	}
-//	public int getBindent() {
-//		return bindent;
-//	}
-//	public void setBindent(int bindent) {
-//		this.bindent = bindent;
-//	}
+
+	public int getBid() {
+		return bid;
+	}
+	public void setBid(int bid) {
+		this.bid = bid;
+	}
+	public String getBname() {
+		return bname;
+	}
+	public void setBname(String bname) {
+		this.bname = bname;
+	}
+	public String getBtitle() {
+		return btitle;
+	}
+	public void setBtitle(String btitle) {
+		this.btitle = btitle;
+	}
+	public String getBcontent() {
+		return bcontent;
+	}
+	public void setBcontent(String bcontent) {
+		this.bcontent = bcontent;
+	}
+	public Timestamp getBdate() {
+		return bdate;
+	}
+	public void setBdate(Timestamp bdate) {
+		this.bdate = bdate;
+	}
+	public int getBhit() {
+		return bhit;
+	}
+	public void setBhit(int bhit) {
+		this.bhit = bhit;
+	}
+	public int getBgroup() {
+		return bgroup;
+	}
+	public void setBgroup(int bgroup) {
+		this.bgroup = bgroup;
+	}
+	public int getBstep() {
+		return bstep;
+	}
+	public void setBstep(int bstep) {
+		this.bstep = bstep;
+	}
+	public int getBindent() {
+		return bindent;
+	}
+	public void setBindent(int bindent) {
+		this.bindent = bindent;
+	}
 //	
 //	
 	
